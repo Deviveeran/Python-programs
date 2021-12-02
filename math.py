6c1b10bf3ffee339b64e044ea16e7d5dc7b83bb7
Durga Devi <91365345+Deviveeran@users.noreply.github.com>
2021-12-02 19:04:13 +0530

    Math Module Methods

---
+++ math.py
@@ -0,0 +1,71 @@
+import math
+
+# Return the arc cosine of numbers
+print(math.acos(0.55))
+print(math.acos(-0.55))
+print(math.acos(0))
+print(math.acos(1))
+print(math.acos(-1)) 
+
+
+import math
+
+# Round a number upward to its nearest integer
+print(math.ceil(1.4))
+print(math.ceil(5.3))
+print(math.ceil(-5.3))
+print(math.ceil(22.6))
+print(math.ceil(10.0)) 
+
+import math
+
+#Return the value of 9 raised to the power of 3
+print(math.pow(9, 3)) 
+
+
+import math
+
+# Return the square root of different numbers
+print (math.sqrt(9))
+print (math.sqrt(25))
+print (math.sqrt(16)) 
+
+import math
+
+# Print error function for different numbers
+print (math.erf(0.67))
+print (math.erf(1.34))
+print (math.erf(-6)) 
+
+
+
+import math
+
+#find the exponential of the specified value
+print(math.exp(65))
+print(math.exp(-6.89)) 
+
+
+import math
+
+# Round numbers down to the nearest integer
+print(math.floor(0.6))
+print(math.floor(1.4))
+print(math.floor(5.3))
+print(math.floor(-5.3))
+print(math.floor(22.6))
+print(math.floor(10.0)) 
+
+
+import math
+
+#Return factorial of a number
+print(math.factorial(9))
+print(math.factorial(6))
+print(math.factorial(12)) 
+
+
+
+
+
+
